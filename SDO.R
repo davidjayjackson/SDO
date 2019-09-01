62d82cf22a9860aa3ee1de0fa9f72206802bc89c
davidjayjackson <davidjayjackson@gmail.com>
2019-09-01 18:26:36 -0400

    SDO Monthly sunspot data. Used tibyverse to read data, filter data by year, plot and create North/South plots and summary tables.

---
+++ SDO.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(lubridate)
+#
+sdo <- read_csv("./ns_sdoh.csv")
+sdo_monthly <- read_csv("./ns_sdoh_mm.csv")
+sdo_monthly$Year <- year(sdo_monthly$Ymd)
+sdo_monthly$Month <- month(sdo_monthly$Ymd)
+sdo_monthly$Day <- day(sdo_monthly$Ymd)
+sdo_monthly$Ymd <- as.Date(sdo_monthly$Ymd)
+tbl_df(sdo_monthly)
+ggplot(data=sdo_monthly,aes(x=Ymd,y=Wn,col="North")) + geom_line() +
+  geom_line(data=sdo_monthly,aes(x=Ymd,y=Ws,col="South")) + ggtitle("SDO N/S Wolf: 2012 - 2019")
+# Current Mimimn 2014 - 2019
+sdo_current <- sdo_monthly %>% filter(Year >= 2014)
+ggplot(data=sdo_current,aes(x=Ymd,y=Wn,col="North")) + geom_line() +
+  geom_line(data=sdo_current,aes(x=Ymd,y=Ws,col="South")) + ggtitle("SDO Current N/S Wolf: 2014 - 2019")
+#
+# Use tidyverse spread function to create crosstab tables.
+# North table for Current Solar Min.
+#
+print("Current(North) Solar Min. 2014-2019")
+sdo_current %>% select(Year,Month,Wn) %>% spread(Month,Wn)
+#
+# South Table for Current Min.
+print("Current(South) Solar Min. 2014-2019")
+sdo_current %>% select(Year,Month,Ws) %>% spread(Month,Ws)
